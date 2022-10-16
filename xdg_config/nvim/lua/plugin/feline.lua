51d98da7bb6c964c6fdce699f17f845ded59fc1b
Simon Jacquet <simon.benoit.jacquet@gmail.com>
2022-10-16 23:04:00 +0200

    Change color of neovim toolbar
    - Colors changed to be less aggressive on the eye

--- xdg_config/nvim/lua/plugin/feline.lua
+++ xdg_config/nvim/lua/plugin/feline.lua
@@ -1,1 +1,19 @@
-require('feline').setup()
+local config = {
+    theme={
+        bg = '#282828',
+        black = '#282828',
+        yellow = '#d8a657',
+        cyan = '#89b482',
+        oceanblue = '#282828',
+        -- oceanblue = '#45707a',
+        green = '#a9b665',
+        orange = '#e78a4e',
+        violet = '#d3869b',
+        magenta = '#c14a4a',
+        white = '#a89984',
+        fg = '#a89984',
+        skyblue = '#7daea3',
+        red = '#ea6962',
+    },
+}
+require('feline').setup(config)
